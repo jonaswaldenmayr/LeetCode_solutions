1354a0399e3da06aca201c52557cefc9bfce8628
jonaswaldenmayr <115011065+jonaswaldenmayr@users.noreply.github.com>
2025-12-22 15:08:19 +0100

    Easy | Time: 76ms (5%) | Space: 17.3 MB (99.7%)

---
+++ 0020-valid-parentheses/solution.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        while "()" in s or "{}" in s or "[]" in s:
+            s = s.replace("()", "")
+            s = s.replace("{}", "")
+            s = s.replace("[]", "")
+        return s == ""
